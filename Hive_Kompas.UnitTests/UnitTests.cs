003917a5b6c628b062651c9f0e9f1e6b5133cb23
Vlad <rutor787893@live.com>
2020-04-11 13:04:48 +0700

    Дописаны Unit тесты.

--- Hive_Kompas.UnitTests/UnitTests.cs
+++ Hive_Kompas.UnitTests/UnitTests.cs
@@ -33,7 +33,7 @@ namespace Hive_Kompas.UnitTests
             Assert.AreEqual(expected, actual, "Геттер HiveLength возвращает неправильное значение.");
         }
 
-        [Test(Description = "Негативный тест геттера Height")]
+        [Test(Description = "Негативный тест геттера Length")]
         public void TestLengthGet_BadValue()
         {
             var wrongHeight = 99999;
@@ -41,5 +41,109 @@ namespace Hive_Kompas.UnitTests
             Assert.Throws<ArgumentException>(() => { hiveParams.HiveLength = wrongHeight; }, "-");
         }
 
+        [Test(Description = "Позитивный тест геттера Width")]
+        public void TestWidthGet_CorrectValue()
+        {
+            var expected = 400;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            hiveParams.HiveWidth = expected;
+            var actual = hiveParams.HiveWidth;
+            Assert.AreEqual(expected, actual, "Геттер HiveWidth возвращает неправильное значение.");
+        }
+
+        [Test(Description = "Негативный тест геттера Width")]
+        public void TestWidthGet_BadValue()
+        {
+            var wrongHeight = 99999;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            Assert.Throws<ArgumentException>(() => { hiveParams.HiveWidth = wrongHeight; }, "-");
+        }
+
+        [Test(Description = "Позитивный тест геттера InLetDiameters")]
+        public void TestInLetDiametersGet_CorrectValue()
+        {
+            var expected = 10;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            hiveParams.InletDiameters = expected;
+            var actual = hiveParams.InletDiameters;
+            Assert.AreEqual(expected, actual, "Геттер InLetDiameters возвращает неправильное значение.");
+        }
+
+        [Test(Description = "Негативный тест геттера InLetDiameters")]
+        public void TestInLetDiametersGet_BadValue()
+        {
+            var wrongHeight = 99999;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            Assert.Throws<ArgumentException>(() => { hiveParams.InletDiameters = wrongHeight; }, "-");
+        }
+        [Test(Description = "Позитивный тест геттера LegHeight")]
+        public void TestLegHeightGet_CorrectValue()
+        {
+            var expected = 400;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            hiveParams.LegHeight = expected;
+            var actual = hiveParams.LegHeight;
+            Assert.AreEqual(expected, actual, "Геттер LegHeight возвращает неправильное значение.");
+        }
+
+        [Test(Description = "Негативный тест геттера LegHeight")]
+        public void TestLegHeightGet_BadValue()
+        {
+            var wrongHeight = 99999;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            Assert.Throws<ArgumentException>(() => { hiveParams.LegHeight = wrongHeight; }, "-");
+        }
+        [Test(Description = "Позитивный тест геттера LegLength")]
+        public void TestLegLengthGet_CorrectValue()
+        {
+            var expected = 400;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            hiveParams.LegLength = expected;
+            var actual = hiveParams.LegLength;
+            Assert.AreEqual(expected, actual, "Геттер LegLength возвращает неправильное значение.");
+        }
+
+        [Test(Description = "Негативный тест геттера LegLength")]
+        public void TestLegLengthGet_BadValue()
+        {
+            var wrongHeight = 99999;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            Assert.Throws<ArgumentException>(() => { hiveParams.LegLength = wrongHeight; }, "-");
+        }
+        [Test(Description = "Позитивный тест геттера LegWidth")]
+        public void TestLegWidthGet_CorrectValue()
+        {
+            var expected = 400;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            hiveParams.LegWidth = expected;
+            var actual = hiveParams.LegWidth;
+            Assert.AreEqual(expected, actual, "Геттер LegWidth возвращает неправильное значение.");
+        }
+
+        [Test(Description = "Негативный тест геттера LegWidth")]
+        public void TestLegWidthGet_BadValue()
+        {
+            var wrongHeight = 99999;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            Assert.Throws<ArgumentException>(() => { hiveParams.LegWidth = wrongHeight; }, "-");
+        }
+
+        [Test(Description = "Позитивный тест геттера RoofThickness")]
+        public void TestRoofThicknessGet_CorrectValue()
+        {
+            var expected = 100;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            hiveParams.RoofThickness = expected;
+            var actual = hiveParams.RoofThickness;
+            Assert.AreEqual(expected, actual, "Геттер RoofThickness возвращает неправильное значение.");
+        }
+
+        [Test(Description = "Негативный тест геттера RoofThickness")]
+        public void TestRoofThicknessGet_BadValue()
+        {
+            var wrongHeight = 99999;
+            var hiveParams = new HiveParams(200, 300, 300, 10, 50, 100, 100, 100);
+            Assert.Throws<ArgumentException>(() => { hiveParams.RoofThickness = wrongHeight; }, "-");
+        }
     }
 }

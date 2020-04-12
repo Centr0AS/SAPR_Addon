bc0cfc5c2ee0d35ebb492d75a2fdbf3966ea75c4
Vlad <rutor787893@live.com>
2020-04-12 11:51:46 +0700

    Подправлен интерфейс.

--- ORSAPR_Project/Main.Designer.cs
+++ ORSAPR_Project/Main.Designer.cs
@@ -107,7 +107,7 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(3, 36);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(352, 272);
+            this.groupBox1.Size = new System.Drawing.Size(344, 272);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Параметры улья";
@@ -346,7 +346,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 371);
+            this.ClientSize = new System.Drawing.Size(350, 371);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
